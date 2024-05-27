d1451ba474e3b7760ac11109a30cb7a2469f26ac
rubekx <rubekx@gmail.com>
2024-05-27 15:37:13 -0300

    update project

--- replication_slot_debezium.sql
+++ replication_slot_debezium.sql
@@ -2,5 +2,5 @@
 -- Check existing replication slots
 SELECT * FROM pg_replication_slots;
 
--- Drop the "debezium" replication slot if it exists and is not in use
-SELECT pg_drop_replication_slot('debezium');
+-- Drop the "mapper_positions" replication slot if it exists and is not in use
+SELECT pg_drop_replication_slot('mapper_positions');
